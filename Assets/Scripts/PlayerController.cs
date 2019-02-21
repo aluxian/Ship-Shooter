009ee0065129660782a11696ef0caa11dd310735
Alexandru Rosianu <me@aluxian.com>
2019-02-21 14:50:27 +0000

    tidy up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [System.Serializable]
 public class Boundary
@@ -13,20 +11,18 @@ public class PlayerController : MonoBehaviour
     //
     // Physics
     //
-    public float rotationModifier; 
+    public float rotationModifier;
     public Vector2 thrustModifier; // (power up, power down)
 
     public float shipSpeed;
 
     public float tiltModifier;
-    
+
     //
     // Shots
     //
     public GameObject shot;
-    [SerializeField]
     public Transform[] shotSpawnsPort;
-   
     public Transform[] shotSpawnsStarboard;
     public float reloadTime;
 
@@ -37,8 +33,8 @@ public class PlayerController : MonoBehaviour
     // Components
     //
 
-    private AudioSource au;
-    private Rigidbody rb;
+    private Rigidbody shipRigidBody;
+    private AudioSource shootAudioSource;
 
     //
     // World
@@ -50,8 +46,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        au = GetComponent<AudioSource>();
+        shipRigidBody = GetComponent<Rigidbody>();
+        shootAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -60,18 +56,20 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButton("Fire1") && Time.time > nextFirePort)
         {
             nextFirePort = Time.time + reloadTime;
-            for(int x = 0; x < shotSpawnsPort.Length; x++) {
+            for (int x = 0; x < shotSpawnsPort.Length; x++)
+            {
                 Instantiate(shot, shotSpawnsPort[x].position, shotSpawnsPort[x].rotation);
-                au.Play();
+                shootAudioSource.Play();
             }
         }
+
         if (Input.GetButton("Fire2") && Time.time > nextFireStarboard)
         {
             nextFireStarboard = Time.time + reloadTime;
             for (int x = 0; x < shotSpawnsPort.Length; x++)
             {
                 Instantiate(shot, shotSpawnsStarboard[x].position, shotSpawnsStarboard[x].rotation);
-                au.Play();
+                shootAudioSource.Play();
             }
         }
     }
@@ -84,13 +82,13 @@ public class PlayerController : MonoBehaviour
 
         if (userThrust > 0)
         {
-            rb.AddRelativeForce(new Vector3(0, 0, (thrustModifier[0] * userThrust)));
+            shipRigidBody.AddRelativeForce(new Vector3(0, 0, thrustModifier[0] * userThrust));
         }
         else
         {
-            rb.AddRelativeForce(new Vector3(0, 0, (thrustModifier[1] * userThrust)));
+            shipRigidBody.AddRelativeForce(new Vector3(0, 0, thrustModifier[1] * userThrust));
         }
 
-        rb.AddTorque(new Vector3(0,rotationModifier * rb.velocity.magnitude * userRudder, 0));
+        shipRigidBody.AddTorque(new Vector3(0, rotationModifier * shipRigidBody.velocity.magnitude * userRudder, 0));
     }
 }
