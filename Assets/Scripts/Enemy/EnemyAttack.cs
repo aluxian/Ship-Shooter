a836df26cc942b35cd84afa40c7be79c37b7b839
Cam <cam@camshepherd.com>
2019-02-27 20:37:40 +0000

    Enemy attacks work

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -6,10 +6,21 @@ public class EnemyAttack : MonoBehaviour
 {
     private GameController gameController;
     public GameObject explosion;
-    public int value;
+    public int strength;
+    public float attackDelay;
+    public float attackRadius;
 
+    private float nextAttack;
+    private float timer;
+
+    protected PlayerHealth playerHealth;
+    protected GameObject player;
     private void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        timer = 0;
+        nextAttack = 0;
         GameObject result = GameObject.FindWithTag("GameController");
         if (result != null)
         {
@@ -23,7 +34,6 @@ public class EnemyAttack : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        
         if (other.tag == "Boundary")
         {
             return;
@@ -32,8 +42,23 @@ public class EnemyAttack : MonoBehaviour
         if (other.tag == "Player")
         {
             // PlayerController player = (PlayerController)other.gameObject;
+            playerHealth.TakeDamage(strength);
             Instantiate(explosion, other.transform.position, transform.rotation);
+            timer = 0;
         }
         
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        if(timer >= nextAttack && distance <= attackRadius)
+        {
+            playerHealth.TakeDamage(strength);
+            timer = 0;
+            nextAttack = attackDelay;
+        }
     }
 }
