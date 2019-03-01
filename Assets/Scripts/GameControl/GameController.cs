8c6ca0ee1bd29fb450d730a6a4e0570d33e24db6
Cam <cam@camshepherd.com>
2019-03-01 08:52:59 +0000

    Game pause functionality working

---
+++ Assets/Scripts/GameControl/GameController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public Text endText;
+
+    protected bool playing;
+    protected bool gamePaused;
+
+    public WindController wind;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playing = true;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playing)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.J) && !gamePaused)
+            {
+                pauseGame();
+                gamePaused = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.J) && gamePaused)
+            {
+                resumeGame();
+                gamePaused = false;
+            }
+        }
+        
+    }
+
+    
+
+    public void endGame()
+    {
+        playing = false;
+    }
+
+    public void pauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void resumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
