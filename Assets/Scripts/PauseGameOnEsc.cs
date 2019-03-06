eab5aa1059d0948c3212772363252f3f20961517
Alexandru Rosianu <me@aluxian.com>
2019-03-06 12:47:52 +0000

    fix line endings

--- Assets/Scripts/PauseGameOnEsc.cs
+++ Assets/Scripts/PauseGameOnEsc.cs
@@ -1,10 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine;
 
 public class PauseGameOnEsc : MonoBehaviour
 {
